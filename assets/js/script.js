ba58d6a4651f4d9f12d117562cb451f9599b35ff
Aaron Wiebe <awiebe1@cox.net>
2021-07-23 11:38:37 -0700

    Campsite and weather APIs working

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,14 @@ var Sector = ["N","NNE","NNE","NE","NE","ENE","ENE","E","E","ESE","ESE","SE","SE
 let loiArray = [];
 let currentLocation = {lat:"",lng:""};
 let npsApiKey = "71YJlvXLD5CwfW1xEAbx30SgczpxdaZPp5HVB1eL";
-let openWeatherApiKey = "3212807212bfb01b6636f32077439307032f440";
-// Open Layerts appears keyless 7E99AF3C-193D-4F89-8AA3-A799590CB89C
+let openWeatherApiKey = "c6372f1324c78c2e38ccaa1ebef5b15c";
+// Open Layerts appears keyless 
 
 
 function gatherCampsites(){
     var searchStr = (document.querySelector("#searchTerm").value).toUpperCase();
  
-    var apiUrl = 'https://developer.nps.gov/api/v1/campgrounds?stateCode=' + searchStr + '&api_key=71YJlvXLD5CwfW1xEAbx30SgczpxdaZPp5HVB1eL'
+    var apiUrl = "https://developer.nps.gov/api/v1/campgrounds?stateCode=" + searchStr + "&api_key=" + npsApiKey;
     fetch(apiUrl)
     .then(campSiteResponse => campSiteResponse.json())
     .then(campSiteResponse =>{
@@ -45,18 +45,18 @@ function gatherCampsites(){
 
 function specCampsites(){
     searchStr = this.id;
-    var apiUrl = 'https://developer.nps.gov/api/v1/campgrounds?id=' + searchStr + '&api_key=71YJlvXLD5CwfW1xEAbx30SgczpxdaZPp5HVB1eL'
+    var apiUrl = "https://developer.nps.gov/api/v1/campgrounds?id=" + searchStr + "&api_key=" + npsApiKey;
     fetch(apiUrl)
     .then(campSiteResponse => campSiteResponse.json())
     .then(campSiteResponse =>{
-      console.log(campSiteResponse);
-      })
-      var api2Url = "https://api.openweathermap.org/data/2.5/onecall?lat="+campSiteResponse.data[0].latitude+"&lon="+campSiteResponse.data[0].longitude+"&exclude=minutely,hourly&appid=c6372f1324c78c2e38ccaa1ebef5b15c"
+      console.log(campSiteResponse)
+      var api2Url = "https://api.openweathermap.org/data/2.5/onecall?lat="+campSiteResponse.data[0].latitude+"&lon="+campSiteResponse.data[0].longitude+"&exclude=minutely,hourly&appid=" +  openWeatherApiKey;
       fetch(api2Url)
       .then(weatherResponse => weatherResponse.json())
       .then(weatherResponse => {
-
+        console.log(weatherResponse)
         //setting 5 day forcast
+
         for (let i = 1; i < 6; i++) {
             document.querySelector("#Day"+i+"-temp").textContent = "Hi Temp: " + (((weatherResponse.daily[i].temp.max-273.15) * (9/5)) + 32).toFixed(2);
             document.querySelector("#Day"+i+"-wind").textContent = "Wind: " + Sector[(Math.round(weatherResponse.daily[i].wind_deg / 11.25))] + "@" + weatherResponse.daily[i].wind_speed.toFixed(0);
@@ -64,6 +64,7 @@ function specCampsites(){
             document.querySelector("#Day"+i+"-icon").src="http://openweathermap.org/img/wn/" + weatherResponse.daily[i].weather[0].icon + "@2x.png";
         };
       });
+    });  
 };
 
 
